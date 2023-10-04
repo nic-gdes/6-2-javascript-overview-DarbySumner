be86880f274b0956eadbac89d0c68018fdab2ba8
Darby <dssumner@nic.edu>
2023-10-04 08:09:42 -0700

    1.2 Variables

--- javascript.js
+++ javascript.js
@@ -1,7 +1,20 @@
 // 1.1 Assignment: Where do you put JavaScript?
-
+    /* You link it in the body */
+ alert("Javascript is easy");
 // 1.2 Assignment: Variables
+    /*John is a 25 year old male from the United Kingdom. */
+    
+    var firstName = "John";
+    console.log(firstName);
+    
+    var lastName = "Smith";
+    console.log(lastName);
 
+    var age = "25";
+    console.log(age);
+
+    var livesIn = "the United Kingdom";
+    console.log(livesIn);
 // 1.3 Assignment: Variable Declaration & Initialization
 
 // 1.4 Assignment: Complete your statements
