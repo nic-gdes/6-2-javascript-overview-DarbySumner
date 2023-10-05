67424dc77b185b39ef081987d17d21c6617ce0ab
Darby <dssumner@nic.edu>
2023-10-04 22:05:11 -0700

    1.4 complete

--- javascript.js
+++ javascript.js
@@ -1,6 +1,8 @@
 // 1.1 Assignment: Where do you put JavaScript?
     /* You link it in the body */
+
  alert("Javascript is easy");
+
 // 1.2 Assignment: Variables
     /*John is a 25 year old male from the United Kingdom. */
     
@@ -15,11 +17,38 @@
 
     var livesIn = "the United Kingdom";
     console.log(livesIn);
+
 // 1.3 Assignment: Variable Declaration & Initialization
+
+var a = "apple";
+console.log(a)
+
+var b = "bologna";
+console.log(b)
+
+var c = "cranberries";
+console.log(c)
+
+var d = "delta";
+console.log(d)
+
+var e;
+
 
 // 1.4 Assignment: Complete your statements
 
+x = 2;
+console.log(x)
+
+y = 3;
+console.log(y)
+
+z = x + y;
+console.log(z)
+
 // 1.5 Assignment: Brackets, Braces and Parentheses
+
+
 
 // 1.6 Assignment: Data Types
 
